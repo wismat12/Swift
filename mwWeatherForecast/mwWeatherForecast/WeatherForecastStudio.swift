f152fd9c09421b52df5bc0e7429f809fbfd8983d
Matteo <matteo@Matteos-iMac.local>
2018-11-10 02:52:16 +0100

    completed

---
+++ mwWeatherForecast/mwWeatherForecast/WeatherForecastStudio.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherForecastStudio.swift
+//  mwWeatherForecast
+//
+//  Created by Matteo on 17/10/2018.
+//  Copyright © 2018 RS KIS.EDU. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForecastStudio: NSObject {
+
+}
